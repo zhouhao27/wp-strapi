f1c0270f6c24ee49de565cbbac190e0f31c6cbb6
Zhou Hao <zhouhao@vps.server.local>
2023-06-20 04:00:42 +0000

    Error handling and use temp token

--- src/api/webhook/controllers/webhook.js
+++ src/api/webhook/controllers/webhook.js
@@ -8,21 +8,30 @@ const axios = require('axios');
 module.exports = {
   postAction: async (ctx, next) => {
     console.log('postAction')
-    console.log(ctx.request.body);
-
+    // console.log(ctx.request.body);
 
     const entries = ctx.request.body.entry;
     console.log(JSON.stringify(entries));
+
+    if (!Array.isArray(entries)) {
+      console.log("No entry found")
+      return;
+    }
 
     // TODO: Constants, refactor the code, complete and error handling
     // parsing data from WhatsApp Cloud API (webhook)
     entries.forEach(entry => {
       const changes = entry.changes
-      // console.log(JSON.stringify(changes));
+      // console.log(JSON.stringify(changes));      
       changes.forEach(change => {
         if (change.field === 'messages') {
           const messages = change.value.messages
           const phoneNumberId = change.value.metadata.phone_number_id
+
+          if (!Array.isArray(messages)) {
+            console.log("No message found")
+            return;
+          }
 
           messages.forEach(message => {
             // console.log(JSON.stringify(message));
@@ -30,6 +39,8 @@ module.exports = {
               const body = message.text.body
               const to = message.from
               const idMessage = message.id
+              const postToken = 'EAAUwoVY47WIBADkczGhw8ZAJnxvXxZANjnaHqbp3viBSLhnamOSKpifT6SrsglLZCKANxlcsrBHemaZCHePlZA38QMD1ZB13UWcjEdxZAlldV6rK9OLh7U5Jo48W6ENZAj1MuuH46qeI0ln1uVDd2j2PwWG43PiKqXrLo9ZAqH1tH0vdi8rM77SBnqZCeRmbeZBIUXNiiiZCWWVH5gZDZD'
+              // permanent token: EAAUwoVY47WIBAHZB1SG1lqmQNdMGPWh1Nf9F9xzSx6F4Wnd9KxhB0HCllMLtBeiL3vDMBofTbi9Lh9CRYmZCgIN0Ov7pvYONSl09GTOzuGIvZCggZA53SIZCnLqDMZCgEDLTvzUzjmZBo86sRp9J6qA8fQM7VHcAqZCHfEKoIF2cG0XVG7COqh10
 
               // send back
               axios({
@@ -38,7 +49,7 @@ module.exports = {
                   "https://graph.facebook.com/v17.0/" +
                   phoneNumberId +
                   "/messages?access_token=" +
-                  'EAAUwoVY47WIBAHZB1SG1lqmQNdMGPWh1Nf9F9xzSx6F4Wnd9KxhB0HCllMLtBeiL3vDMBofTbi9Lh9CRYmZCgIN0Ov7pvYONSl09GTOzuGIvZCggZA53SIZCnLqDMZCgEDLTvzUzjmZBo86sRp9J6qA8fQM7VHcAqZCHfEKoIF2cG0XVG7COqh10',
+                  postToken,
                 data: {
                   messaging_product: "whatsapp",
                   to: to,
